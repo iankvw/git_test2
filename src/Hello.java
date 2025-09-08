5753fc0469d74aebf8f89c0bbca653f8d9da2b09
Ian Kim <ink@kakao.com>
2025-09-08 22:43:14 +0900

    delete src/Hello.java delete src/Main.java

--- src/Hello.java
+++
@@ -1,8 +0,0 @@
-public class Hello {
-
-    String message = "안녕하세요"
-
-    public void say() {
-        System.out.println(message);
-    }
-}
--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        Hello h =  new Hello();
-        h.say();
-    }
-}
